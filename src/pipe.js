acd8690ef342bce0e3c5b94732332c3d90125be4
wyssyw <1545869244@qq.com>
2022-04-04 00:08:32 +0800

    我爱才让草

--- src/pipe.js
+++ src/pipe.js
@@ -1,6 +1,7 @@
 // CommonJs导入通过给原型添加方法和属性那样添加属性和方法的module.exports对象, 对象引用不变
 const { num, id, sayHi } = require('../mock/pipeData'); // 对象解构
 console.log(num, id, sayHi());
+console.log('i love cairangcao');
 
 
 // CommonJs导入被直接赋值的module.exports对象, module.exports对象地址可能会被改
