9fb961bdc3a277485a02d9ce36c0fc9aba474a83
wyssyw <1545869244@qq.com>
2022-04-04 12:31:39 +0800

    测试CommonJs规范的module.exports 和 require

--- src/CommonJs.js
+++ src/CommonJs.js
@@ -1,9 +1,9 @@
 // CommonJs导入通过给原型添加方法和属性那样添加属性和方法的module.exports对象, 对象引用不变
-const { num, id, sayHi } = require('../mock/pipeData'); // 对象解构
+const { num, id, sayHi } = require('../mock/CommonJs'); // 对象解构
 console.log(num, id, sayHi());
 console.log('i love cairangcao');
 
 
 // CommonJs导入被直接赋值的module.exports对象, module.exports对象地址可能会被改
-// const res = require('../mock/pipeData');
+// const res = require('../mock/CommonJs');
 // console.log(res, typeof res);
